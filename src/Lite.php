0794024eb0078b8ef6f7cd276dab4e6498534bfd
dogstar <chanzonghuang@gmail.com>
2017-11-22 21:01:15 +0800

    up

--- src/Lite.php
+++ src/Lite.php
@@ -8,6 +8,37 @@ namespace PhalApi\Pinyin;
  * @author: dogstar 2017-11-22
  */
 
+use Overtrue\Pinyin\Pinyin;
+
 class Lite {
 
+    protected $pinyin;
+
+    public function __construct() {
+        $this->pinyin = new Pinyin();
+    }
+
+    public function convert($string, $option = Pinyin::NONE) {
+        return $this->pinyin->convert($string, $option);
+    }
+
+    public function name($stringName, $option = Pinyin::NONE) {
+        return $this->pinyin->name($stringName, $option);
+    }
+
+    public function permalink($string, $delimiter = '-') {
+        return $this->pinyin->permalink($string, $delimiter);
+    }
+
+    public function abbr($string, $delimiter = '') {
+        return $this->pinyin->abbr($string, $delimiter);
+    }
+
+    public function phrase($string, $delimiter = ' ', $option = Pinyin::NONE) {
+        return $this->pinyin->phrase($string, $delimiter, $option);
+    }
+
+    public function sentence($sentence, $withTone = false) {
+        return $this->pinyin->sentence($sentence, $withTone);
+    }
 }
